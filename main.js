72a87dd45a412594457413bcc51beaa494cc7bed
SAVANOo <savio_gato1000@hotmail.com>
2024-02-28 08:51:08 -0300

    Finalizando seção projetos

--- main.js
+++ main.js
@@ -7,19 +7,17 @@ function changeContent(sectionId) {
         section.classList.remove('active');
     });
 
-    // Adiciona a classe 'active' à seção correspondente ao ID passado como parâmetro
-    var selectedSection = document.getElementById(sectionId);
-    selectedSection.classList.add('active');
-
-    // Obtém todos os itens de navegação
+    // Remove a classe 'Navigation-Item-Active' de todos os itens de navegação
     var navItems = document.querySelectorAll('.Navigation-Item');
-
-    // Remove a classe 'active' de todos os itens de navegação
     navItems.forEach(function (navItem) {
         navItem.classList.remove('Navigation-Item-Active');
     });
 
+    // Adiciona a classe 'active' à seção correspondente ao ID passado como parâmetro
+    var selectedSection = document.getElementById(sectionId);
+    selectedSection.classList.add('active');
+
     // Adiciona a classe 'Navigation-Item-Active' ao item de navegação correspondente ao ID passado como parâmetro
-    var selectedNavItem = document.getElementById('navigation' + sectionId.charAt(0).toUpperCase() + sectionId.slice(1));
+    var selectedNavItem = document.getElementById('navigation' + sectionId);
     selectedNavItem.classList.add('Navigation-Item-Active');
 }
